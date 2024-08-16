d9ab08be3f34589b088c72c354e75ad6a0d6a1d1
ZaeRon007 <julien.pelissier8@orange.fr>
2024-08-16 18:49:24 +0200

    Rename of user & rentals

---
+++ src/main/java/com/chatop/model/DBRentals.java
@@ -0,0 +1,47 @@
+package com.chatop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "rentals")
+public class DBRentals {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    
+    @Column(name = "surface")
+    private int surface;
+    
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "picture")
+    private String picture;
+    
+    @Column(name = "description")
+    private String description;
+    
+    @Column(name = "owner_id")
+    private int owner_id;
+    
+    @Column(name = "created_at")
+    private String created_at;
+    
+    @Column(name = "updated_at")
+    private String updated_at;
+}
---
+++ src/main/java/com/chatop/model/DBUser.java
@@ -0,0 +1,42 @@
+package com.chatop.model;
+
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class DBUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "created_at")
+    private String created_at;
+    
+    @Column(name = "updated_at")
+    private String updated_at;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "token")
+    private String token;
+
+}
